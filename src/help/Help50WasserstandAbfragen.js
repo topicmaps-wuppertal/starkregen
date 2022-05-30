8419dfe331056027f27d95ed5c39656df4ed97e6
helllth <th@cismet.de>
2022-05-30 12:30:27 +0200

    text changes due to customer requests

--- src/help/Help50WasserstandAbfragen.js
+++ src/help/Help50WasserstandAbfragen.js
@@ -24,7 +24,7 @@ const Component = () => {
             Fließgeschwindigkeit", zunächst mit einem kurzen Bedienungshinweis. Ein Klick auf eine
             beliebige Position in der Karte bewirkt jetzt, dass die Zelle in der Karte markiert und
             der zugehörige Maximalwert der Wassertiefe bzw. der Fließgeschwindigkeit in diesem Feld
-            angezeigt wird. Die Anzeige der maximalen Wassertiefen wird dabei auf volle 10 cm
+            angezeigt wird. Die Anzeige der maximalen Wassertiefe wird dabei auf volle 10 cm
             gerundet (z. B. "ca. 90 cm"), um die{" "}
             <a onClick={() => setAppMenuActiveMenuSection("aussagekraft")}>
               beschränkte Aussagekraft der Simulationsergebnisse
@@ -37,16 +37,16 @@ const Component = () => {
           <p>
             <b>Tipp für die Abfrage der maximalen Fließgeschwindigkeiten:</b> Deaktivieren Sie die{" "}
             <a onClick={() => setAppMenuActiveMenuSection("karteninhalt")}>Animation</a> und stellen
-            Sie einen sehr großen Betrachtungsmaßstab ein (Zoomstufe 17 oder 18). Dann wird Ihnen in
-            der Kartendarstellung auch die zu der maximalen Fließgeschwindigkeit gehörende
-            Fließrichtung zellenscharf angezeigt.
+            Sie einen sehr großen Betrachtungsmaßstab ein (Zoomstufe 22). Dann wird Ihnen in der
+            Kartendarstellung auch die zu der maximalen Fließgeschwindigkeit gehörende Fließrichtung
+            zellenscharf angezeigt.
           </p>
           <p>
-            Wenn Sie nach der Abfrage eines maximalen Wassertiefees bzw. einer maximalen
-            Fließgeschwindigkeit eine andere Simulation auswählen, wird der angezeigte Zellwert
-            automatisch aktualisiert. So können Sie für eine bestimmte Position bequem alle
-            angebotenen Simulations-Szenarien durchgehen. Auch im Abfragemodus können Sie die Karte
-            mit gedrückter linker Maustaste verschieben. Wenn Sie auf diese Weise oder durch{" "}
+            Wenn Sie nach der Abfrage einer maximalen Wassertiefe bzw. Fließgeschwindigkeit eine
+            andere Simulation auswählen, wird der angezeigte Zellwert automatisch aktualisiert. So
+            können Sie für eine bestimmte Position bequem alle angebotenen Simulations-Szenarien
+            durchgehen. Auch im Abfragemodus können Sie die Karte mit gedrückter linker Maustaste
+            verschieben. Wenn Sie auf diese Weise oder durch{" "}
             <a onClick={() => setAppMenuActiveMenuSection("positionieren")}>
               Positionierung über einen Suchbegriff
             </a>{" "}
