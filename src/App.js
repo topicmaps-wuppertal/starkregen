2aff116dbf201becc18e11db7135f22e8ed245ef
Thorsten Hell <thorsten.hell@cismet.de>
2023-01-16 13:36:22 +0100

    remopve "alpha" from app title

--- src/App.js
+++ src/App.js
@@ -110,7 +110,7 @@ function App() {
             ]}
             menuFooter={
               <Footer
-                appName="Starkregengefahrenkarte Wuppertal (Alpha)"
+                appName="Starkregengefahrenkarte Wuppertal"
                 version={getApplicationVersion()}
                 hintergrundkartenText="True Orthophoto 2022, Amtliche Basiskarte (ABK), Hillshade © Stadt Wuppertal | Stadtkarte 2.0 © RVR | WebAtlasDE © BKG"
                 taglineModelling={
@@ -140,7 +140,7 @@ function App() {
         homeZoom={18}
         homeCenter={[51.27202324060668, 7.20162372978018]}
         modeSwitcherTitle="Starkregengefahrenkarte (Alpha)"
-        documentTitle="Starkregengefahrenkarte (Alpha) Wuppertal"
+        documentTitle="Starkregengefahrenkarte Wuppertal"
         gazData={gazData}
       />
     </TopicMapContextProvider>
